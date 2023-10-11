9dc735bacefc26e48ec23f48ab5c94f9185c959b
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2023-10-12 01:28:22 +0300

    Update index.js

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,23 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decodedData(encoded, translations) {
+  return encoded.map((item) => {
+    const decodedItem = {};
+    const excludeItem = ["groupId", "service", "formatSize", "ca"];
 
+    for (let key in item) {
+      if (key.endsWith("Id") && !excludeItem.includes(key)) {
+        const idValue = item[key];
+        decodedItem[key] = translations[idValue] || idValue;
+      } else {
+        decodedItem[key] = item[key];
+      }
+    }
+    return decodedItem;
+  });
+}
 
-// console.log(decoded)
+const decoded = decodedData(encoded, translations);
+
+console.log(decoded);
